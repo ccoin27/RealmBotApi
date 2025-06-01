4ae39d8e905a643c70ba42f003da2f60dc8be796
ccoin27 <coin8020@gmail.com>
2025-06-01 17:22:13 +0200

    Update server.js

--- server.js
+++ server.js
@@ -3,25 +3,20 @@ export default {
       const { pathname } = new URL(req.url);
       const parts = pathname.split('/');
   
-      // Параметры из URL
       const enemy = parts[1];
-      const animation = parts[2]?.replace('.gif', '');  // убираем .gif из имени анимации
+      const animation = parts[2]?.replace('.gif', ''); 
   
       if (!enemy || !animation) {
         return new Response("Некорректный запрос", { status: 400 });
       }
   
       try {
-        // Формируем путь к файлу на GitHub
         const filePath = `sprites/enemies/${enemy}/${animation}.gif`;
   
-        // Формируем URL для получения файла с GitHub (замени на свой репозиторий)
         const githubRawUrl = `https://raw.githubusercontent.com/ccoin27/rpg_api/main/${filePath}`;
   
-        // Получаем файл с GitHub
         const file = await fetch(githubRawUrl);
   
-        // Если файл найден, возвращаем его содержимое
         if (file.ok) {
           const arrayBuffer = await file.arrayBuffer();
           return new Response(arrayBuffer, {
